91e750e82296efdc5f9f766d4172a015920cdce2
Asha JS <94jsasha@gmail.com>
2021-01-07 20:35:03 +0100

    Create chat messages pagination

--- chat-backend/controllers/chatController.js
+++ chat-backend/controllers/chatController.js
@@ -32,7 +32,7 @@ exports.index = async (req, res) => {
       }
     ]
   })
-  return res.send(user.Chats)
+  return res.json(user.Chats)
 }
 
 exports.create = async (req, res) => {
@@ -103,10 +103,38 @@ exports.create = async (req, res) => {
       ]
     })
 
-    return res.send(chatNew)
+    return res.json(chatNew)
 
   } catch (e) {
     await t.rollback()
     return res.status(500).json({ status: 'Error', message: e.message })
   }
+}
+
+
+exports.messages = async (req, res) => {
+  const limit = 10
+  const page = req.query.page || 1
+  const offset = page > 1 ? page * limit : 0
+  const messages = await Message.findAndCountAll({
+    where: {
+      chatId: req.query.id
+    },
+    limit,
+    offset
+  })
+
+  const totalPages = Math.ceil(messages.count / limit)
+
+  if (page > totalPages)
+    return res.json({ data: { messages: [] } })
+
+  const result = {
+    messages: messages.rows,
+    pagination: {
+      page,
+      totalPages
+    }
+  }
+  return res.json(result)
 }

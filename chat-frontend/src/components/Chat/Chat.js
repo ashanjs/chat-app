0e66f8c6def8c2f6ba6aa9d05c086d7903666ceb
Asha JS <94jsasha@gmail.com>
2021-01-07 21:18:56 +0100

    fetch chats and save in store

--- chat-frontend/src/components/Chat/Chat.js
+++ chat-frontend/src/components/Chat/Chat.js
@@ -1,11 +1,19 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import Navbar from './components/Navbar/Navbar'
-
+import { fetchChats } from '../../store/actions/chat'
 import './chat.scss'
 
 const Chat = () => {
+
+  const dispatch = useDispatch()
   const user = useSelector(state => state.authReducer.user)
+
+  useEffect(() => {
+    dispatch(fetchChats())
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  }, [dispatch])
 
   return (
     <div id="chat-container">
---
+++ chat-frontend/src/services/chatService.js
@@ -0,0 +1,16 @@
+import API from './api'
+
+const ChatService = {
+
+  fetchChats: () => {
+    return API.get('/chats')
+      .then(({ data }) => {
+        return data
+      })
+      .catch(err => {
+        throw err
+      })
+  }
+}
+
+export default ChatService

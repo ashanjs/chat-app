b56c51b3f5b8240a051e3e0010fe857680b35d7d
Asha JS <94jsasha@gmail.com>
2021-01-07 19:43:36 +0100

    Create new chat function

--- chat-backend/controllers/chatController.js
+++ chat-backend/controllers/chatController.js
@@ -4,6 +4,7 @@ const Chat = models.Chat
 const ChatUser = models.ChatUser
 const Message = models.Message
 const { Op } = require('sequelize')
+const { sequelize } = require('../models')
 
 exports.index = async (req, res) => {
   const user = await User.findOne({
@@ -34,3 +35,78 @@ exports.index = async (req, res) => {
   return res.send(user.Chats)
 }
 
+exports.create = async (req, res) => {
+
+  const { partnerId } = req.body
+  const t = await sequelize.transaction()
+
+  try {
+    const user = await User.findOne({
+      where: {
+        id: req.user.id
+      },
+      include: [
+        {
+          model: Chat,
+          where: {
+            type: 'dual'
+          },
+          include: [
+            {
+              model: ChatUser,
+              where: {
+                userId: partnerId
+              }
+            }
+          ]
+        }
+      ]
+    })
+    if (user && user.Chats.length > 0) {
+      return res.status(403).json({
+        status: 'Error',
+        message: 'Chat with this user already exist!'
+      })
+    }
+
+    const chat = await Chat.create({ type: 'dual' }, { transaction: t })
+
+    await ChatUser.bulkCreate([
+      {
+        chatId: chat.id,
+        userId: req.user.id
+      },
+      {
+        chatId: chat.id,
+        userId: partnerId
+      }
+    ], { transaction: t })
+
+    await t.commit()
+
+    const chatNew = await Chat.findOne({
+      where: {
+        id: chat.id
+      },
+      include: [
+        {
+          model: User,
+          where: {
+            [Op.not]: {
+              id: req.user.id
+            }
+          }
+        },
+        {
+          model: Message
+        }
+      ]
+    })
+
+    return res.send(chatNew)
+
+  } catch (e) {
+    await t.rollback()
+    return res.status(500).json({ status: 'Error', message: e.message })
+  }
+}

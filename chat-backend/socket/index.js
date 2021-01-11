db37dabdde79385f233d7836d220c997d60c0182
Asha JS <94jsasha@gmail.com>
2021-01-11 13:02:22 +0100

    Disconnect user socket

--- chat-backend/socket/index.js
+++ chat-backend/socket/index.js
@@ -1,6 +1,8 @@
 const socketIo = require('socket.io')
 const { sequelize } = require('../models')
+
 const users = new Map()
+const userSockets = new Map()
 
 const SocketServer = (server) => {
   const io = socketIo(server, {
@@ -18,9 +20,11 @@ const SocketServer = (server) => {
         existingUser.sockets = [...existingUser.sockets, ...[socket.id]]
         users.set(user.id, existingUser)
         sockets = [...existingUser.sockets, ...[socket.id]]
+        userSockets.set(socket.id, user.id)
       } else {
         users.set(user.id, { id: user.id, sockets: [socket.id] })
         sockets.push(socket.id)
+        userSockets.set(socket.id, user.id)
       }
 
       const onlineFriends = [] //ids
@@ -31,7 +35,7 @@ const SocketServer = (server) => {
       for (let i = 0; i < chatters.length; i++) {
         if (users.has(chatters[i])) {
           const chatter = users.get(chatters[i])
-          chatters.sockets.forEach(socket => {
+          chatter.sockets.forEach(socket => {
             try {
               io.to(socket).emit('online', user)
             } catch (e) { }
@@ -47,6 +51,37 @@ const SocketServer = (server) => {
         } catch (e) { }
       })
 
+    })
+
+    socket.on('disconnect', async () => {
+      const user = user.get(userSockets.get(socket.id))
+
+      if (user.sockets.length > 1) {
+
+        user.sockets = user.sockets.filter(sock => {
+          if (sock !== socket.id) return true
+
+          userSockets.delete(sock)
+          return false
+        })
+        users.set(user.id, user)
+      } else {
+
+        const chatters = await getChatters(user.id)
+
+        for (let i = 0; i < chatters.length; i++) {
+          if (users.has(chatters[i])) {
+            users.get(chatters[i]).sockets.forEach(socket => {
+              try {
+                io.to(socket).emit('offline', user)
+              } catch (e) { }
+            })
+          }
+        }
+
+        userSockets.delete(socket.id)
+        users.delete(user.id)
+      }
     })
   })
 }

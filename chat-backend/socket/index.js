99e3d45984ac6964931a01ccdca8c17be3b911ac
Asha JS <94jsasha@gmail.com>
2021-01-11 11:21:21 +0100

    Add socket on join event

--- chat-backend/socket/index.js
+++ chat-backend/socket/index.js
@@ -1,13 +1,51 @@
 const socketIo = require('socket.io')
 
+const users = new Map()
+
 const SocketServer = (server) => {
-  const io = socketIo(server)
+  const io = socketIo(server, {
+    cors: {
+      origin: '*',
+    }
+  })
 
   io.on('connection', (socket) => {
-
     socket.on('join', async (user) => {
 
-      console.log('New user joined: ', user.firstName)
+      let sockets = []
+      if (users.has(user.id)) {
+        const existingUser = users.get(user.id)
+        existingUser.sockets = [...existingUser.sockets, ...[socket.id]]
+        users.set(user.id, existingUser)
+        sockets = [...existingUser.sockets, ...[socket.id]]
+      } else {
+        users.set(user.id, { id: user.id, sockets: [socket.id] })
+        sockets.push(socket.id)
+      }
+
+      const onlineFriends = [] //ids
+      const chatters = [] //query
+
+      // notify his friends that user is now online
+      for (let i = 0; i < chatters.length; i++) {
+        if (users.has(chatters[i])) {
+          const chatter = users.get(chatters[i])
+          chatters.sockets.forEach(socket => {
+            try {
+              io.to(socket).emit('online', user)
+            } catch (e) { }
+          })
+          onlineFriends.push(chatter.id)
+        }
+      }
+
+      // send to user sockets which of his friends are online
+      sockets.forEach(socket => {
+        try {
+          io.to(socket).emit('friends', onlineFriends)
+        } catch (e) { }
+      })
+
     })
   })
 }

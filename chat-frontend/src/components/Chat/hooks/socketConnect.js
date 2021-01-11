c34b081f0ffe1c7881354a2144bdd4615e9884e4
Asha JS <94jsasha@gmail.com>
2021-01-11 14:33:19 +0100

    Update state of all online friends

--- chat-frontend/src/components/Chat/hooks/socketConnect.js
+++ chat-frontend/src/components/Chat/hooks/socketConnect.js
@@ -1,15 +1,30 @@
 import { useEffect } from 'react'
 import socketIOClient from 'socket.io-client'
+import { onlineFriends } from '../../../store/actions/chat'
 
 function useSocket(user, dispatch) {
 
   useEffect(() => {
 
     const socket = socketIOClient.connect('http://127.0.0.1:3000')
+
     socket.emit('join', user)
 
     socket.on('typing', (user) => {
       console.log('Event', user)
+    })
+
+    socket.on('friends', (friends) => {
+      console.log('Friends', friends)
+      dispatch(onlineFriends(friends))
+    })
+
+    socket.on('online', (user) => {
+      console.log('Online', user)
+    })
+
+    socket.on('offline', (user) => {
+      console.log('Offline', user)
     })
 
   }, [dispatch])
--- chat-frontend/src/store/reducers/chat.js
+++ chat-frontend/src/store/reducers/chat.js
@@ -1,4 +1,4 @@
-import { FETCH_CHATS, SET_CURRENT_CHAT } from '../actions/chat'
+import { FETCH_CHATS, SET_CURRENT_CHAT, FRIENDS_ONLINE, FRIEND_ONLINE, FRIEND_OFFLINE } from '../actions/chat'
 
 const initialState = {
   chats: [],
@@ -18,6 +18,26 @@ const chatReducer = (state = initialState, action) => {
         ...state,
         currentChat: payload
       }
+    case FRIENDS_ONLINE:
+      const chatsCopy = state.chats.map(chat => {
+        return {
+          ...chat,
+          Users: chat.Users.map(user => {
+            if (payload.includes(user.id)) {
+              return {
+                ...user,
+                status: 'online'
+              }
+            }
+            return user
+          })
+        }
+      })
+      return {
+        ...state,
+        chats: chatsCopy
+      }
+
     default:
       return state
   }

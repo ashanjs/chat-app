3dd167ffed565694fb42a555e8f98b6e098ab70b
Asha JS <94jsasha@gmail.com>
2021-01-13 13:38:47 +0100

    create Add friend modal

--- chat-frontend/src/components/Chat/components/FriendList/FriendList.js
+++ chat-frontend/src/components/Chat/components/FriendList/FriendList.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState, Fragment } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Friend from '../Friend/Friend'
 import { setCurrentChat } from '../../../../store/actions/chat'
+import Modal from '../../../Modal/Modal'
 import './FriendList.scss'
 
 const FriendList = () => {
 
   const dispatch = useDispatch()
   const chats = useSelector(state => state.chatReducer.chats)
+
+  const [showFriendModal, setShowFriendModal] = useState(false)
+  const [suggestions, setSuggestions] = useState([])
+
   const openChat = (chat) => {
     dispatch(setCurrentChat(chat))
+  }
+
+  const searchFriends = (e) => {
+    // chat service
+  }
+
+  const addNewFriend = (id) => {
+    // dispatch
   }
 
   return (
@@ -30,6 +43,32 @@ const FriendList = () => {
             : <p id='no-chat'>No friends added</p>
         }
       </div>
+      {
+        showFriendModal &&
+        <Modal>
+          <Fragment key='header'>
+            <h3 className='m-0'>create new chat</h3>
+          </Fragment>
+          <Fragment key='body'>
+            <p>Find friends by typing their name below</p>
+            <input
+              onInput={e => searchFriends(e)}
+              type='text'
+              placeholder='Search...'
+            />
+            <div id='suggestions'>
+              {
+                suggestions.map(user => {
+                  return <div>
+                    <p className='m-0'>{user.firstName} {user.lastName}</p>
+                    <button onClick={() => addNewFriend(user.id)}>ADD</button>
+                  </div>
+                })
+              }
+            </div>
+          </Fragment>
+        </Modal>
+      }
     </div>
   )
 }

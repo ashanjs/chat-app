ddfae89e537c99844a16c20c1ca76665e587cfc3
Asha JS <94jsasha@gmail.com>
2021-01-18 15:52:14 +0100

    install cypress testing library

--- chat-frontend/cypress/e2e/chat-app.js
+++ chat-frontend/cypress/e2e/chat-app.js
@@ -1,10 +1,8 @@
 describe('chat-app', () => {
   it('login to chat app', () => {
     cy.visit('/')
-    cy.get('.mb-1 > input').type('john.doe@gmail.com')
-    cy.get('.mb-2 > input').type('secret')
-    cy.get('button').click()
-    cy.get('#profile-menu > :nth-child(2)').click()
-    cy.get('#profile-options > :nth-child(2)').click()
+    cy.findByPlaceholderText('Email').type('john.doe@gmail.com')
+    cy.findByPlaceholderText('Password').type('secret')
+    cy.findByRole('button').click()
   })
 })
